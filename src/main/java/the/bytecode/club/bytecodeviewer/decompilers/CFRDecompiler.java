a9e69505d4263d83be395b474e10bbca8c90911f
afffsdd <afffsdd@users.noreply.github.com>
2016-03-12 19:07:47 -0500

    Update CFR and Fernflower

--- src/main/java/the/bytecode/club/bytecodeviewer/decompilers/CFRDecompiler.java
+++ src/main/java/the/bytecode/club/bytecodeviewer/decompilers/CFRDecompiler.java
@@ -195,7 +195,7 @@ public class CFRDecompiler extends Decompiler {
                     e1.analyseTop(dcCommonState);
                     TypeUsageCollector collectingDumper = new TypeUsageCollector(e1);
                     e1.collectTypeUsages(collectingDumper);
-                    d = new FileDumper(output.toAbsolutePath().toString(), e1.getClassType(), summaryDumper, collectingDumper.getTypeUsageInformation(), options, illegalIdentifierDump);
+                    d = new FileDumper(output.toAbsolutePath().toString(), true, e1.getClassType(), summaryDumper, collectingDumper.getTypeUsageInformation(), options, illegalIdentifierDump);
                     e1.dump(d);
                     d.print("\n");
                     d.print("\n");
