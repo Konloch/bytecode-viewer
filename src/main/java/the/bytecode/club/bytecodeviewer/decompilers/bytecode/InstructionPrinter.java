c209db4f5bca807685d1b3da5e1b82a2bc5ea405
Konloch <konloch@gmail.com>
2021-07-01 04:34:40 -0700

    Instruction Printing On Malware Scanner

--- src/main/java/the/bytecode/club/bytecodeviewer/decompilers/bytecode/InstructionPrinter.java
+++ src/main/java/the/bytecode/club/bytecodeviewer/decompilers/bytecode/InstructionPrinter.java
@@ -69,6 +69,8 @@ public class InstructionPrinter {
 
     protected List<AbstractInsnNode> matchedInsns;
     protected Map<LabelNode, Integer> labels;
+    private boolean firstLabel = false;
+    private ArrayList<String> info = new ArrayList<>();
 
     public InstructionPrinter(MethodNode m, TypeAndName[] args) {
         this.args = args;
@@ -99,64 +101,17 @@ public class InstructionPrinter {
      * @return The print as an ArrayList
      */
     public ArrayList<String> createPrint() {
-        ArrayList<String> info = new ArrayList<>();
+        firstLabel = false;
+        info.clear();
         ListIterator<?> it = mNode.instructions.iterator();
-        boolean firstLabel = false;
         while (it.hasNext()) {
             AbstractInsnNode ain = (AbstractInsnNode) it.next();
-            String line = "";
-            if (ain instanceof VarInsnNode) {
-                line = printVarInsnNode((VarInsnNode) ain);
-            } else if (ain instanceof IntInsnNode) {
-                line = printIntInsnNode((IntInsnNode) ain);
-            } else if (ain instanceof FieldInsnNode) {
-                line = printFieldInsnNode((FieldInsnNode) ain);
-            } else if (ain instanceof MethodInsnNode) {
-                line = printMethodInsnNode((MethodInsnNode) ain);
-            } else if (ain instanceof LdcInsnNode) {
-                line = printLdcInsnNode((LdcInsnNode) ain);
-            } else if (ain instanceof InsnNode) {
-                line = printInsnNode((InsnNode) ain);
-            } else if (ain instanceof JumpInsnNode) {
-                line = printJumpInsnNode((JumpInsnNode) ain);
-            } else if (ain instanceof LineNumberNode) {
-                line = printLineNumberNode();
-            } else if (ain instanceof LabelNode) {
-                if (firstLabel
-                        && BytecodeViewer.viewer.appendBracketsToLabels
-                        .isSelected())
-                    info.add("}");
-
-                line = printLabelnode((LabelNode) ain);
-
-                if (BytecodeViewer.viewer.appendBracketsToLabels.isSelected()) {
-                    if (!firstLabel)
-                        firstLabel = true;
-                    line += " {";
-                }
-            } else if (ain instanceof TypeInsnNode) {
-                line = printTypeInsnNode((TypeInsnNode) ain);
-            } else if (ain instanceof FrameNode) {
-                line = printFrameNode((FrameNode) ain);
-            } else if (ain instanceof IincInsnNode) {
-                line = printIincInsnNode((IincInsnNode) ain);
-            } else if (ain instanceof TableSwitchInsnNode) {
-                line = printTableSwitchInsnNode((TableSwitchInsnNode) ain);
-            } else if (ain instanceof LookupSwitchInsnNode) {
-                line = printLookupSwitchInsnNode((LookupSwitchInsnNode) ain);
-            } else if (ain instanceof InvokeDynamicInsnNode) {
-                line = printInvokeDynamicInsNode((InvokeDynamicInsnNode) ain);
-            } else if (ain instanceof MultiANewArrayInsnNode) {
-                line = printMultiANewArrayInsNode((MultiANewArrayInsnNode) ain);
-            } else {
-                line += "UNADDED OPCODE: " + nameOpcode(ain.getOpcode()) + " "
-                        + ain;
-            }
+            String line = printInstruction(ain);
             if (!line.isEmpty()) {
                 if (match)
                     if (matchedInsns.contains(ain))
                         line = "   -> " + line;
-
+        
                 info.add(line);
             }
         }
@@ -164,6 +119,60 @@ public class InstructionPrinter {
                 && BytecodeViewer.viewer.appendBracketsToLabels.isSelected())
             info.add("}");
         return info;
+    }
+    
+    public String printInstruction(AbstractInsnNode ain)
+    {
+        String line = "";
+        if (ain instanceof VarInsnNode) {
+            line = printVarInsnNode((VarInsnNode) ain);
+        } else if (ain instanceof IntInsnNode) {
+            line = printIntInsnNode((IntInsnNode) ain);
+        } else if (ain instanceof FieldInsnNode) {
+            line = printFieldInsnNode((FieldInsnNode) ain);
+        } else if (ain instanceof MethodInsnNode) {
+            line = printMethodInsnNode((MethodInsnNode) ain);
+        } else if (ain instanceof LdcInsnNode) {
+            line = printLdcInsnNode((LdcInsnNode) ain);
+        } else if (ain instanceof InsnNode) {
+            line = printInsnNode((InsnNode) ain);
+        } else if (ain instanceof JumpInsnNode) {
+            line = printJumpInsnNode((JumpInsnNode) ain);
+        } else if (ain instanceof LineNumberNode) {
+            line = printLineNumberNode();
+        } else if (ain instanceof LabelNode) {
+            if (firstLabel
+                    && BytecodeViewer.viewer.appendBracketsToLabels
+                    .isSelected())
+                info.add("}");
+        
+            line = printLabelnode((LabelNode) ain);
+        
+            if (BytecodeViewer.viewer.appendBracketsToLabels.isSelected()) {
+                if (!firstLabel)
+                    firstLabel = true;
+                line += " {";
+            }
+        } else if (ain instanceof TypeInsnNode) {
+            line = printTypeInsnNode((TypeInsnNode) ain);
+        } else if (ain instanceof FrameNode) {
+            line = printFrameNode((FrameNode) ain);
+        } else if (ain instanceof IincInsnNode) {
+            line = printIincInsnNode((IincInsnNode) ain);
+        } else if (ain instanceof TableSwitchInsnNode) {
+            line = printTableSwitchInsnNode((TableSwitchInsnNode) ain);
+        } else if (ain instanceof LookupSwitchInsnNode) {
+            line = printLookupSwitchInsnNode((LookupSwitchInsnNode) ain);
+        } else if (ain instanceof InvokeDynamicInsnNode) {
+            line = printInvokeDynamicInsNode((InvokeDynamicInsnNode) ain);
+        } else if (ain instanceof MultiANewArrayInsnNode) {
+            line = printMultiANewArrayInsNode((MultiANewArrayInsnNode) ain);
+        } else {
+            line += "UNADDED OPCODE: " + nameOpcode(ain.getOpcode()) + " "
+                    + ain;
+        }
+        
+        return line;
     }
 
     protected String printVarInsnNode(VarInsnNode vin) {
