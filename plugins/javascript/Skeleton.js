590115d899670866044f64bc8d90c74e72b4eb01
Konloch <konloch@gmail.com>
2022-01-17 18:58:52 -0600

    Example Update

--- plugins/javascript/Skeleton.js
+++ plugins/javascript/Skeleton.js
@@ -1,6 +1,6 @@
 function execute(classNodeList) {
     var PluginConsole = Java.type("the.bytecode.club.bytecodeviewer.api.PluginConsole");
-    var gui = new PluginConsole("Skeleton");
+    var gui = new PluginConsole("Skeleton Title");
     gui.setVisible(true);
-    gui.appendText("executed skeleton");
+    gui.appendText("executed skeleton example");
 }
