4272282c664b63c704d44cbafe9357dc3e090375
Konloch <konloch@gmail.com>
2022-01-22 13:09:53 -0600

    Code Cleanup

--- plugins/java/Skeleton.java
+++ plugins/java/Skeleton.java
@@ -2,6 +2,12 @@ import java.util.List;
 import org.objectweb.asm.tree.ClassNode;
 import the.bytecode.club.bytecodeviewer.api.*;
 
+/**
+ ** This is a skeleton template for BCV's Java Plugin System
+ **
+ ** @author [Your Name Goes Here]
+ **/
+
 public class Skeleton extends Plugin {
 
     @Override
