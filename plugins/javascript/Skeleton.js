ecf8e435414fc6280c2e11807fc265c526fd38dc
Konloch <konloch@gmail.com>
2022-01-22 13:09:24 -0600

    Code Cleanup

--- plugins/javascript/Skeleton.js
+++ plugins/javascript/Skeleton.js
@@ -1,3 +1,10 @@
+
+/**
+** This is a skeleton template for BCV's Javascript Plugin System
+**
+** @author [Your Name Goes Here]
+**/
+
 function execute(classNodeList) {
     var PluginConsole = Java.type("the.bytecode.club.bytecodeviewer.api.PluginConsole");
     var gui = new PluginConsole("Skeleton Title");
