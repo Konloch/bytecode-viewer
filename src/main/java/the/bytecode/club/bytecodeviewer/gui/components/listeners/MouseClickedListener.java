68da28093344b439c2ad03c4052c6759f854c18c
Konloch <konloch@gmail.com>
2024-09-25 20:09:07 -0600

    Class Cleanup

--- src/main/java/the/bytecode/club/bytecodeviewer/gui/components/listeners/MouseClickedListener.java
+++ src/main/java/the/bytecode/club/bytecodeviewer/gui/components/listeners/MouseClickedListener.java
@@ -18,6 +18,7 @@
 
 package the.bytecode.club.bytecodeviewer.gui.components.listeners;
 
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -25,28 +26,20 @@ import java.awt.event.MouseListener;
  * @author Konloch
  * @since 6/25/2021
  */
-public class MouseClickedListener implements MouseListener
+public class MouseClickedListener extends MouseAdapter
 {
 	private final MouseClickedEvent mouseClickedEvent;
 	
-	public MouseClickedListener(MouseClickedEvent mouseClickedEvent) {this.mouseClickedEvent = mouseClickedEvent;}
+	public MouseClickedListener(MouseClickedEvent mouseClickedEvent)
+	{
+		this.mouseClickedEvent = mouseClickedEvent;
+	}
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		mouseClickedEvent.mouseClicked(e);
+		super.mouseClicked(e);
 	}
-	
-	@Override
-	public void mouseEntered(MouseEvent arg0) { }
-	
-	@Override
-	public void mouseExited(MouseEvent arg0) { }
-	
-	@Override
-	public void mousePressed(MouseEvent arg0) { }
-	
-	@Override
-	public void mouseReleased(MouseEvent e) { }
 	
 	public interface MouseClickedEvent
 	{
