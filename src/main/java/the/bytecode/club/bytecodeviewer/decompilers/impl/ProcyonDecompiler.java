f121870272f85db62d53a616740a71f5b210889c
Konloch <konloch@gmail.com>
2024-10-02 12:13:36 -0600

    Procyon Refactoring

--- src/main/java/the/bytecode/club/bytecodeviewer/decompilers/impl/ProcyonDecompiler.java
+++ src/main/java/the/bytecode/club/bytecodeviewer/decompilers/impl/ProcyonDecompiler.java
@@ -58,27 +58,6 @@ public class ProcyonDecompiler extends AbstractDecompiler
         super("Procyon Decompiler", "proycon");
     }
 
-    public DecompilerSettings getDecompilerSettings()
-    {
-        DecompilerSettings settings = new DecompilerSettings();
-        settings.setAlwaysGenerateExceptionVariableForCatchBlocks(BytecodeViewer.viewer.alwaysGenerateExceptionVars.isSelected());
-        settings.setExcludeNestedTypes(BytecodeViewer.viewer.excludeNestedTypes.isSelected());
-        settings.setShowDebugLineNumbers(BytecodeViewer.viewer.showDebugLineNumbers.isSelected());
-        settings.setIncludeLineNumbersInBytecode(BytecodeViewer.viewer.includeLineNumbersInBytecode.isSelected());
-        settings.setIncludeErrorDiagnostics(BytecodeViewer.viewer.includeErrorDiagnostics.isSelected());
-        settings.setShowSyntheticMembers(BytecodeViewer.viewer.showSyntheticMembers.isSelected());
-        settings.setSimplifyMemberReferences(BytecodeViewer.viewer.simplifyMemberReferences.isSelected());
-        settings.setMergeVariables(BytecodeViewer.viewer.mergeVariables.isSelected());
-        settings.setForceExplicitTypeArguments(BytecodeViewer.viewer.forceExplicitTypeArguments.isSelected());
-        settings.setForceExplicitImports(BytecodeViewer.viewer.forceExplicitImports.isSelected());
-        settings.setFlattenSwitchBlocks(BytecodeViewer.viewer.flattenSwitchBlocks.isSelected());
-        settings.setRetainPointlessSwitches(BytecodeViewer.viewer.retainPointlessSwitches.isSelected());
-        settings.setRetainRedundantCasts(BytecodeViewer.viewer.retainRedunantCasts.isSelected());
-        settings.setUnicodeOutputEnabled(BytecodeViewer.viewer.unicodeOutputEnabled.isSelected());
-        settings.setJavaFormattingOptions(JavaFormattingOptions.createDefault());
-        return settings;
-    }
-
     @Override
     public String decompileClassNode(ClassNode cn, byte[] bytes)
     {
@@ -244,6 +223,27 @@ public class ProcyonDecompiler extends AbstractDecompiler
                 }
             }
         }
+    }
+
+    public DecompilerSettings getDecompilerSettings()
+    {
+        DecompilerSettings settings = new DecompilerSettings();
+        settings.setAlwaysGenerateExceptionVariableForCatchBlocks(BytecodeViewer.viewer.alwaysGenerateExceptionVars.isSelected());
+        settings.setExcludeNestedTypes(BytecodeViewer.viewer.excludeNestedTypes.isSelected());
+        settings.setShowDebugLineNumbers(BytecodeViewer.viewer.showDebugLineNumbers.isSelected());
+        settings.setIncludeLineNumbersInBytecode(BytecodeViewer.viewer.includeLineNumbersInBytecode.isSelected());
+        settings.setIncludeErrorDiagnostics(BytecodeViewer.viewer.includeErrorDiagnostics.isSelected());
+        settings.setShowSyntheticMembers(BytecodeViewer.viewer.showSyntheticMembers.isSelected());
+        settings.setSimplifyMemberReferences(BytecodeViewer.viewer.simplifyMemberReferences.isSelected());
+        settings.setMergeVariables(BytecodeViewer.viewer.mergeVariables.isSelected());
+        settings.setForceExplicitTypeArguments(BytecodeViewer.viewer.forceExplicitTypeArguments.isSelected());
+        settings.setForceExplicitImports(BytecodeViewer.viewer.forceExplicitImports.isSelected());
+        settings.setFlattenSwitchBlocks(BytecodeViewer.viewer.flattenSwitchBlocks.isSelected());
+        settings.setRetainPointlessSwitches(BytecodeViewer.viewer.retainPointlessSwitches.isSelected());
+        settings.setRetainRedundantCasts(BytecodeViewer.viewer.retainRedunantCasts.isSelected());
+        settings.setUnicodeOutputEnabled(BytecodeViewer.viewer.unicodeOutputEnabled.isSelected());
+        settings.setJavaFormattingOptions(JavaFormattingOptions.createDefault());
+        return settings;
     }
 
     /**
